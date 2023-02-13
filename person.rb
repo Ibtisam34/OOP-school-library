20fe53dc42472da23b3c231f1494607a2a0f676e
unknown <Riyanagwl@gmail.com>
2023-02-13 15:09:32 +0300

    add files

--- person.rb
+++ person.rb
@@ -1,20 +1,14 @@
-class Person
-  attr_accessor :id, :name, :age
+# frozen_string_literal: true
 
-  def initialize(name = 'Unknown', age = nil, parent_permission: true)
-    @id = rand(1..100)
+require_relative 'nameable'
+class Person < Nameable
+  attr_reader :name
+
+  def initialize(name)
     @name = name
-    @age = age
-    @parent_permission = parent_permission
   end
 
-  private
-
-  def of_age?
-    @age >= 18 if @age
-  end
-
-  def can_use_services?
-    of_age? || @parent_permission
+  def correct_name
+    @name
   end
 end
