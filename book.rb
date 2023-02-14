046e5d36977cea174bca033c3b98bef04f535f39
unknown <Riyanagwl@gmail.com>
2023-02-14 17:36:29 +0300

    modified files

--- book.rb
+++ book.rb
@@ -1,8 +1,12 @@
 class Book
-  attr_accesor :title, :author
-
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
+  end
+   attr_accessor :title, :author, :rentals
+
+  def add_rental(date, person)
+    @rentals.push(Rental.new(date, self, person))
   end
 end
--- student.rb
+++ student.rb
@@ -1,7 +1,18 @@
-class Student
-  attr_accesor :name, :classroom
+require 'person'
+class Student < Person
+ 
+  def initialize(name = 'Unknown', age = nil, parent_permission: true, classroom: nil)
+    super(name, age, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+      attr_reader :classroom
 
-  def initialize(name)
-    @name = name
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
