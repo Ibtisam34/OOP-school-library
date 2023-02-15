5dadda04486bf2b2bc2f1ed30ae8d19f3bd28e7e
unknown <Riyanagwl@gmail.com>
2023-02-15 21:05:11 +0300

    update files

--- app.rb
+++ app.rb
@@ -2,51 +2,70 @@ require './book'
 require './student'
 require './teacher'
 require './rental'
-
 class App
   def initialize
     @books = []
-    @student = []
-    @teacher = []
-    @rental = []
-    @people = []
+    @students = []
+    @teachers = []
+    @rentals = []
   end
 
   def list_books
-    @books.each_with_index { |book, index| puts "(#{index}) Title: #{book.title}, Author: #{book.author} " }
-  end
-
-  def list_people
-    @student.each { |student| puts " [Student] Name: #{student.name}, Age: #{student.age}, ID:#{student.id} " }
-    @teacher.each { |teacher| puts " [Teacher]  Name: #{teacher.name}, Age: #{teacher.age}, ID:#{teacher.id}" }
-  end
-
-  def show_people
-    @people = @student + @teacher
-    @people.each_with_index do |people, index|
-      puts "(#{index}) Name: #{people.name}, Age: #{people.age}, ID:#{people.id}"
+    @books.each_with_index do |book, index|
+      puts "(#{index}) Title: #{book.title}, Author: #{book.author}"
     end
   end
 
-  def show_rental
+  def list_people
+    @students.each do |student|
+      puts "[Student] Name: #{student.name}, Age: #{student.age}, ID: #{student.id}"
+    end
+    @teachers.each do |teacher|
+      puts "[Teacher] Name: #{teacher.name}, Age: #{teacher.age}, ID: #{teacher.id}"
+    end
+  end
+
+  def show_people
+    @people = @students + @teachers
+    @people.each_with_index do |person, index|
+      puts "(#{index}) Name: #{person.name}, Age: #{person.age}, ID: #{person.id}"
+    end
+  end
+
+  def show_rentals
     print 'ID of person: '
     id = gets.chomp.to_i
     puts 'Rentals:'
-    @rental.each { |rent| puts " Date: #{rent.date} Book: #{rent.book.title} " if rent.person.id == id }
+    rentals = @rentals.select { |rental| rental.person.id == id }
+    rentals.each do |rental|
+      puts " Date: #{rental.date} Book: #{rental.book.title}"
+    end
   end
 
   def create_rental
     puts 'Select a book from the following list by number'
     list_books
     book_id = gets.chomp.to_i
-    puts "\nSelect a person from the following list by number (not id)"
-    show_people
-    person_id = gets.chomp.to_i
-    print "\nDate: "
-    date = gets.chomp
-    rent = Rental.new(@people[person_id], @books[book_id], date)
-    @rental.push(rent)
-    puts "Rental created successfully\n\n"
+    # Check that the book with the specified ID exists in the @books array
+    if @books[book_id]
+      @book = @books[book_id] # assign selected book to @book
+      puts "\nSelect a person from the following list by number (not id)"
+      show_people
+      person_id = gets.chomp.to_i
+      # Check that the person with the specified ID exists in the @people array
+      if @people[person_id]
+        print "\nDate: "
+        date = gets.chomp
+        rental = Rental.new(@people[person_id], @book, date)
+        @book.rentals.push(rental) unless @book.rentals.include?(rental)
+        @rentals.push(rental)
+        puts "Rental created successfully\n\n"
+      else
+        puts 'Invalid person ID'
+      end
+    else
+      puts 'Invalid book ID'
+    end
   end
 
   def create_book
@@ -60,31 +79,40 @@ class App
   end
 
   def create_person
-    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     num = gets.chomp
     case num
     when '1'
-      print 'Age:'
-      age = gets.chomp
-      print 'Name:'
-      name = gets.chomp
-      print 'Parent permisssion [y/n]:'
-      permission = gets.chomp
-      student = Student.new(1, age, name, parent_permission: permission)
-      @student.push(student)
-      puts "Person created successfully\n\n"
+      create_student
     when '2'
-      print 'Age:'
-      age = gets.chomp
-      print 'Name:'
-      name = gets.chomp
-      print 'Specialization:'
-      specialz = gets.chomp
-      teacher = Teacher.new(age, specialz, name, parent_permission: true)
-      @teacher.push(teacher)
-      puts "Person created successfully\n\n"
+      create_teacher
     else
-      puts 'invalid'
+      puts 'Invalid option'
     end
+  end
+
+  def create_student
+    print 'Age: '
+    age = gets.chomp
+    print 'Name: '
+    name = gets.chomp
+    print 'Has parent permission: [Y/N]? '
+    parent_permission = gets
+    permission = true if parent_permission.downcase == 'y'
+    permission = false if parent_permission.downcase == 'n'
+    @students.push(Student.new('Unknown', age, name, parent_permission: permission))
+    puts 'Person created successfully'
+  end
+
+  def create_teacher
+    print 'Age:'
+    age = gets.chomp
+    print 'Name:'
+    name = gets.chomp
+    print 'Specialization:'
+    specialization = gets.chomp
+    teacher = Teacher.new(age, specialization, name)
+    @teachers.push(teacher)
+    puts "Teacher created successfully\n\n"
   end
 end
--- main.rb
+++ main.rb
@@ -23,13 +23,13 @@ class Main
     when '2'
       app.list_people
     when '3'
-      app.create_teacher_student
+      app.create_person
     when '4'
       app.create_book
     when '5'
       app.create_rental
     when '6'
-      app.show_rental
+      app.show_rentals
     when '7'
       puts puts "Thank you for using this app!\n\n"
       break
